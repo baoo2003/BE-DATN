7321290147f2fe7065f1c85eaa7d8ac0ffcccedc
Bảo Vũ <vuquocbao673@gmail.com>
2025-12-03 17:58:00 +0700

    update: db with ssl

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,7 +5,7 @@ import { NewsModule } from './modules/news/news.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthModule } from './modules/auth/auth.module';
 import { UserModule } from './modules/user/user.module';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { S3Module } from './modules/s3/s3.module';
 
 @Module({
@@ -15,18 +15,30 @@ import { S3Module } from './modules/s3/s3.module';
       envFilePath: ['.env'],
       expandVariables: true,
     }),
-    TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: process.env.DB_HOST,
-      port: Number(process.env.DB_PORT),
-      username: process.env.DB_USERNAME,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB_NAME,
-      autoLoadEntities: true,
-      synchronize: true,
-      charset: 'utf8mb4',
-      extra: {
-        charset: 'utf8mb4_unicode_ci',
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => {
+        const caDecoded = Buffer.from(
+          config.get<string>('DB_CA') ?? '',
+          'base64',
+        ).toString('utf-8');
+
+        return {
+          type: 'mysql',
+          host: config.get<string>('DB_HOST'),
+          port: Number(config.get<string>('DB_PORT')),
+          username: config.get<string>('DB_USERNAME'),
+          password: config.get<string>('DB_PASSWORD'),
+          database: config.get<string>('DB_NAME'),
+          autoLoadEntities: true,
+          synchronize: true,
+          charset: 'utf8mb4',
+          extra: { charset: 'utf8mb4_unicode_ci' },
+          ssl: {
+            ca: caDecoded,
+            rejectUnauthorized: true,
+          },
+        };
       },
     }),
     NewsModule,
