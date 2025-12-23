3bba158b96f25e7a0bdc7773ff7b609409eccf73
Bảo Vũ <vuquocbao673@gmail.com>
2025-12-23 18:12:53 +0700

    update: enum

--- src/modules/news/enums/news.enum.ts
+++ src/modules/news/enums/news.enum.ts
@@ -1,12 +1,15 @@
 export enum Topic {
-  KinhTe = 'kinh tế',
-  TheGioi = 'thế giới',
-  GiaoDuc = 'giáo dục',
-  SucKhoeDoiSong = 'sức khỏe đời sống',
-  CongNghe = 'công nghệ',
-  PhapLuat = 'pháp luật',
-  Xe = 'xe',
-  TheThao = 'thể thao',
-  VanHoaGiaiTri = 'văn hóa giải trí',
-  ChinhTri = 'chính trị',
+  CongNghe = 'công nghệ', // 0
+  DuLich = 'du lịch', // 1
+  GiaoDuc = 'giáo dục', // 2
+  GiaiTri = 'giải trí', // 3
+  KhoaHoc = 'khoa học', // 4
+  KinhDoanh = 'kinh doanh', // 5
+  PhapLuat = 'pháp luật', // 6
+  SucKhoe = 'sức khỏe', // 7
+  TheGioi = 'thế giới', // 8
+  TheThao = 'thể thao', // 9
+  ThoiSu = 'thời sự', // 10
+  Xe = 'xe', // 11
+  DoiSong = 'đời sống', // 12
 }
